4f1aa6424c5d624ddbef77eeb69865d5729b0747
Sha1050 <90437356+Sha1050@users.noreply.github.com>
2022-03-25 01:27:21 -0600

    v2

--- materials/lab/2/bhg-scanner/scanner/scanner_test.go
+++ materials/lab/2/bhg-scanner/scanner/scanner_test.go
@@ -1,78 +1,30 @@
-// bhg-scanner/scanner.go modified from Black Hat Go > CH2 > tcp-scanner-final > main.go
-// Code : https://github.com/blackhat-go/bhg/blob/c27347f6f9019c8911547d6fc912aa1171e6c362/ch-2/tcp-scanner-final/main.go
-// License: {$RepoRoot}/materials/BHG-LICENSE
-// Useage:
-// {TODO 1: FILL IN}
-
 package scanner
 
 import (
-	"fmt"
-	"net"
-	"sort"
-	"time"
+	"testing"
 )
-func worker(ports, results chan int) {
-	for p := range ports {
-		address := fmt.Sprintf("scanme.nmap.org:%d", p)    
 
-		conn, err := net.DialTimeout("tcp", address, 10 * time.Second ) 
-		// conn, err := net.Dial("tcp", address) // TODO 2 : REPLACE THIS WITH DialTimeout (before testing!)
-		if err != nil { 
-			results <- -1 * p
-			continue
-		}
-		conn.Close()
-		results <- p
-	}
+// THESE TESTS ARE LIKELY TO FAIL IF YOU DO NOT CHANGE HOW the worker connects (e.g., you should use DialTimeout)
+func TestOpenPort(t *testing.T){
+
+    got := PortScanner() // Currently function returns only number of open ports
+    want := 2 // default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns 
+	          //consider what would happen if you parameterize the portscanner address and ports to scan
+
+    if got != want {
+        t.Errorf("got %d, wanted %d", got, want)
+    }
 }
 
-// for Part 5 - consider
-// easy: taking in a variable for the ports to scan (int? slice? ); a target address (string?)?
-// med: easy + return  complex data structure(s?) (maps or slices) containing the ports.
-// hard: restructuring code - consider modification to class/object 
-// No matter what you do, modify scanner_test.go to align; note the single test currently fails
-func PortScanner() (int, int){  
+func TestTotalPortsScanned(t *testing.T){
+	// THIS TEST WILL FAIL - YOU MUST MODIFY THE OUTPUT OF PortScanner()
 
-	//TODO 3 : ADD closed ports; currently code only tracks open ports
-	var openports []int  // notice the capitalization here. access limited!
-	var closedports []int
-	ports := make(chan int, 100)   // TODO 4: TUNE THIS FOR CODEANYWHERE / LOCAL MACHINE
-	results := make(chan int)
+    got := PortScanner() // Currently function returns only number of open ports
+    want := 1024 // default value; consider what would happen if you parameterize the portscanner ports to scan
 
-	for i := 0; i < cap(ports); i++ {
-		go worker(ports, results)
-	}
-
-	go func() {
-		for i := 1; i <= 1024; i++ {
-			ports <- i
-		}
-	}()
-
-	for i := 0; i < 1024; i++ {
-		port := <-results
-		if port > 0 {
-			openports = append(openports, port)
-		} else if port < 0{
-			closedports = append(closedports, -1*port)
-		}
-	}
-
-	close(ports)
-	close(results)
-	sort.Ints(openports)
-
-	//TODO 5 : Enhance the output for easier consumption, include closed ports
-
-	for _, port := range openports {
-		fmt.Printf("%d open\n", port)
-	}
-	for _, port := range closedports {
-		fmt.Printf("%d open\n", port)
-	}
-
-
-	return len(openports), len(closedports)// TODO 6 : Return total number of ports scanned (number open, number closed); 
-	//you'll have to modify the function parameter list in the defintion and the values in the scanner_test
+    if got != want {
+        t.Errorf("got %d, wanted %d", got, want)
+    }
 }
+
+
